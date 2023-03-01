a19fedaab3d8179d498af415dba66d6d0475182f
Joe Fogiato <joe.fogiato@gmail.com>
2023-03-01 13:13:34 -0500

    Add: Room class and associated tests

---
+++ test/Room-test.js
@@ -0,0 +1,41 @@
+import chai from 'chai';
+import Room from '../src/classes/Room';
+import testData from './test-data';
+
+const expect = chai.expect;
+
+describe('Room', function() {
+  let room;
+
+  beforeEach( () => {
+    room = new Room(testData.rooms[0])
+  });
+
+  it('Should be an instance of Room', () => {
+    expect(room).to.be.an.instanceOf(Room);
+  });
+
+  it('Should have a number', () => {
+    expect(room.number).to.equal(1);
+  });
+
+  it('Should have a type', () => {
+    expect(room.roomType).to.equal('residential suite');
+  });
+
+  it('Should know whether or not it has a bidet', () => {
+    expect(room.bidet).to.equal(true);
+  });
+
+  it('Should have a bed size', () => {
+    expect(room.bedSize).to.equal('queen');
+  });
+
+  it('Should have the number of beds', () => {
+    expect(room.numBeds).to.equal(1);
+  });
+
+  it('Should have a cost per night', () => {
+    expect(room.costPerNight).to.equal(358.4);
+  });
+});
