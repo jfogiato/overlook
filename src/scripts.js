79b6d244d36a8d3d9b07d702244242f6293e2aee
Joe Fogiato <joe.fogiato@gmail.com>
2023-03-01 13:51:56 -0500

    Add: Initial DOM & global variables

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,24 @@
+// IMPORTS 📥
 import './css/styles.css';
 import './images/hotel-logo.png';
 import './images/hotel-room.png';
+import User from './classes/User';
+import Booking from './classes/Booking';
+import Room from './classes/Room';
 
+// DOM VARIABLES 🖥️
+const userName = document.getElementById('userName');
+const totalSpent = document.getElementById('totalSpent');
+const totalRewards = document.getElementById('totalRewards');
+const modalSection = document.getElementById('modalSection');
+const miniRoomCards = document.getElementById('miniRoomCards');
+const upcomingMinis = document.getElementById('upcomingMinis');
+const pastMinis = document.getElementById('pastMinis');
 
+const searchButton = document.getElementById('searchButton');
+const filter = document.getElementById('filters');
+
+// GLOBAL VARIABLES 🌍
+const currentUser;
+const bookings;
+const rooms;
